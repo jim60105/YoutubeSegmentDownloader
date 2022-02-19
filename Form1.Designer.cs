59abee1bb26be277ff5f35053ba536766a53270c
陳鈞 <jim60105@gmail.com>
2022-02-20 04:03:01 +0800

    Finish Download yt-dlp & FFmpeg

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace YoutubeSegmentDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_checking_ytdlp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_checking_ffmpeg = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.label_checking_ytdlp);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label_checking_ffmpeg);
+            this.panel1.Location = new System.Drawing.Point(200, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // label_checking_ytdlp
+            // 
+            this.label_checking_ytdlp.AutoSize = true;
+            this.label_checking_ytdlp.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_checking_ytdlp.Location = new System.Drawing.Point(341, 0);
+            this.label_checking_ytdlp.Name = "label_checking_ytdlp";
+            this.label_checking_ytdlp.Size = new System.Drawing.Size(59, 41);
+            this.label_checking_ytdlp.TabIndex = 5;
+            this.label_checking_ytdlp.Text = "❌";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(0, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Downloading FFmpeg";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Downloading yt-dlp";
+            // 
+            // label_checking_ffmpeg
+            // 
+            this.label_checking_ffmpeg.AutoSize = true;
+            this.label_checking_ffmpeg.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_checking_ffmpeg.Location = new System.Drawing.Point(341, 69);
+            this.label_checking_ffmpeg.Name = "label_checking_ffmpeg";
+            this.label_checking_ffmpeg.Size = new System.Drawing.Size(59, 41);
+            this.label_checking_ffmpeg.TabIndex = 6;
+            this.label_checking_ffmpeg.Text = "❌";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Youtube Segment Downloader";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label_checking_ytdlp;
+        private Label label_checking_ffmpeg;
+        private Panel panel2;
+    }
+}
