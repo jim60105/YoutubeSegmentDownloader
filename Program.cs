760d34a06e7f95a952563532186efcb43790c940
陳鈞 <jim60105@gmail.com>
2022-02-22 05:07:52 +0800

    Adjust Publish profile

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.Win32;
 using Serilog;
 using Serilog.Core;
 using Serilog.Formatting.Display;
@@ -20,9 +21,47 @@ internal static class Program
                         .WriteToSimpleAndRichTextBox(new MessageTemplateTextFormatter("{Message} {Exception}\n"))
                         .CreateLogger();
 
+        Task.Run(SetAddRemoveProgramsIcon);
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
     }
+
+#pragma warning disable CS8602 // 可能 null 參考的取值 (dereference)。
+    private static void SetAddRemoveProgramsIcon()
+    {
+        //if (ApplicationDeployment.IsNetworkDeployed && ApplicationDeployment.CurrentDeployment.IsFirstRun)
+        {
+            try
+            {
+                var iconSourcePath = Path.Combine(System.Windows.Forms.Application.StartupPath, @"YoutubeSegmentDownloader.ico");
+
+                if (!File.Exists(iconSourcePath)) return;
+
+                var uninstallKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+                if (uninstallKey == null) return;
+
+                var subKeyNames = uninstallKey.GetSubKeyNames();
+                foreach (var subkeyName in subKeyNames)
+                {
+                    var myKey = uninstallKey.OpenSubKey(subkeyName, true);
+                    var myValue = myKey.GetValue("DisplayName");
+                    if (myValue != null 
+                        && myValue.ToString() == "Youtube Segment Downloader")
+                    {
+                        myKey.SetValue("DisplayIcon", iconSourcePath);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e.Message);
+            }
+        }
+    }
+#pragma warning restore CS8602 // 可能 null 參考的取值 (dereference)。
 }
+
